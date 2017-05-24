0cb30a022df572073f3471e8801b17f860276528
JA <it5811423122@gmail.com>
2017-05-24 16:24:18 +0700

    Finish 23May

--- app/src/main/java/pkru/nackchainaeramit/patsiri/mypkru2/MainActivity.java
+++ app/src/main/java/pkru/nackchainaeramit/patsiri/mypkru2/MainActivity.java
@@ -1,13 +1,63 @@
 package pkru.nackchainaeramit.patsiri.mypkru2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import static pkru.nackchainaeramit.patsiri.mypkru2.R.id.btnLogin;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    //Explicit
+    private EditText userEditText, passwordEditText;
+    private TextView textView;
+    private Button button;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        super.onCreate(savedInstanceState); //Statement
+        setContentView(R.layout.activity_main); //Statement
+
+        //Initial View
+        initialView();
+
+        //Controller
+        controller();
+
+
+    }   // Main Method
+
+    private void controller() {
+        textView.setOnClickListener(this);
+        button.setOnClickListener(this);
     }
-}
+
+    private void initialView() {
+        userEditText = (EditText) findViewById(R.id.edtUser);
+        passwordEditText = (EditText) findViewById(R.id.edtPassword);
+        textView = (TextView) findViewById(R.id.txtNewRegister);
+        button = (Button) findViewById(R.id.btnLogin);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        //For TextView
+        if (v == textView) {
+            //Intent to NewRegister
+            Intent intent = new Intent(MainActivity.this,NewRegisterActivity.class);
+            startActivity(intent);
+        }
+
+        //for Button
+        if (v == button) {
+        }
+
+
+    }
+}   // Main Class นี่คือ คลาสหลัก
---
+++ app/src/main/java/pkru/nackchainaeramit/patsiri/mypkru2/NewRegisterActivity.java
@@ -0,0 +1,121 @@
+package pkru.nackchainaeramit.patsiri.mypkru2;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+public class NewRegisterActivity extends AppCompatActivity implements View.OnClickListener {
+
+    //Explicit
+    private EditText nameEditText, userEditText, passwordEditText;
+    private ImageView backImageView, humanImageView, cameraImageView;
+    private Button button;
+    private Uri humanUri, camaraUri;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_register);
+
+        //Initial View
+        initialView();
+
+        //Controller
+        controller();
+
+    }  // main Method
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        //For Human
+        if ((requestCode == 0) && (resultCode == RESULT_OK)) {
+            Log.d("24MayV1", "Human OK");
+
+            //Show Image
+            humanUri = data.getData();
+            try {
+
+                Bitmap bitmap = BitmapFactory.decodeStream(getContentResolver()
+                        .openInputStream(humanUri));
+                humanImageView.setImageBitmap(bitmap);
+            } catch (Exception e) {
+                Log.d("24MayV1","e humanUri ==> " + e.toString());
+            }
+        }   // if human
+
+        // for Camera
+        if ((requestCode == 1)&&(resultCode == RESULT_OK)) {
+
+            Log.d("24MayV1", "Camera Result OK");
+
+            //Show Image
+            camaraUri = data.getData();
+            try {
+
+                Bitmap bitmap = BitmapFactory.decodeStream(getContentResolver()
+                        .openInputStream(camaraUri));
+                humanImageView.setImageBitmap(bitmap);
+
+            } catch (Exception e) {
+                Log.d("24MayV1", " e camera ==> " + e.toString());
+            }
+
+        }  // if Camera
+
+    }   // onActivity
+
+    private void controller() {
+        backImageView.setOnClickListener(this);
+        humanImageView.setOnClickListener(this);
+        cameraImageView.setOnClickListener(this);
+        button.setOnClickListener(this);
+    }
+
+    private void initialView() {
+        nameEditText = (EditText) findViewById(R.id.edtName);
+        userEditText = (EditText) findViewById(R.id.edtUser);
+        passwordEditText = (EditText) findViewById(R.id.edtpassword);
+        backImageView = (ImageView) findViewById(R.id.btnBack);
+        humanImageView = (ImageView) findViewById(R.id.imvHumen);
+        cameraImageView = (ImageView) findViewById(R.id.imvCamera);
+        button = (Button) findViewById(R.id.btnRegister);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        //For Back
+        if (v == backImageView){
+            finish();
+        }
+
+        //For Human
+        if (v == humanImageView) {
+            Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+            intent.setType("image/*");
+            startActivityForResult(Intent.createChooser(intent, "Please Choose App for Choose Image"), 0);
+        }
+
+        //For Camera
+        if (v == cameraImageView) {
+            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            startActivityForResult(intent, 1);
+
+
+        }
+    }
+    // main Class
+}
+
